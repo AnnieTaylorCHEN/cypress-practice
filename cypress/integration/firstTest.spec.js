c44086a1ef230929355a5db0ca18258f1c77245f
AnnieTaylorChen <annietaylorchen@gmail.com>
2020-11-09 13:55:07 +0100

    add more test options.

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const { rgb } = require("d3-color")
+
 describe('our first suite', ()=> {
     it('first test', () => {
         cy.visit('/')
@@ -34,7 +36,7 @@ describe('our first suite', ()=> {
         cy.get('[data-cy="imputEmail1"]')
     })
 
-    it.only('second test', () => {
+    it('second test', () => {
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -55,6 +57,131 @@ describe('our first suite', ()=> {
         .click()
 
         cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
+
+    })
+
+    it('then and wrap methods', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+        cy.contains('nb-card','Using the Grid').then( firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })
+        })
+    })
+
+    it('invoke command', ()=> {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        cy.get('[for="exampleInputEmail1"]').then( label => {
+            expect(label.text()).to.equal('Email address')
+        })
+
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form').find('nb-checkbox').click().find('.custom-checkbox').invoke('attr', 'class').then(classValue => {
+            expect(classValue).to.contain('checked')
+        })
+    })
+
+    it('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('25').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Nov 25, 2020')
+        })
+    })
+
+    it('radio button', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons)
+            .first()
+            .check({force: true})
+            .should('be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(1)
+            .check({force: true})
+
+            cy.wrap(radioButtons)
+            .first()
+            .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(2)
+            .should('be.disabled')
+        })
+    })
+
+    it('check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        //check method can not uncheck, if you want to uncheck use the click method
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+        cy.get('[type="checkbox"]').eq(1).check({force:true})
+    })
+
+    it.only('lists and dropdowns', () => {
+        cy.visit('/')
+
+        //approach 1
+
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+        cy.get('nb-layout-header nav').should('have.css','background-color', 'rgb(34, 43, 69)')
+
+        //approach 2
+        cy.get('nav nb-select').then( dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                if (index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+                
+            })
+        })
 
     })
 })
